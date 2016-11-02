fb255aaab68980c72bead53f32e4fd9be04d22c8
TUJEFA101 <rafa_rock_96@hotmail.com>
2016-11-02 12:13:05 -0700

    Initial commit

---
+++ PracticaFragments-master/app/src/main/java/com/example/ham/practicafragments/FragmentCallbacks.java
@@ -0,0 +1,6 @@
+package com.example.ham.practicafragments;
+
+
+public interface FragmentCallbacks {
+    public void onMsgFromMainToFragment(String strValue);
+}
---
+++ PracticaFragments-master/app/src/main/java/com/example/ham/practicafragments/MainCallbacks.java
@@ -0,0 +1,6 @@
+package com.example.ham.practicafragments;
+
+
+public interface MainCallbacks {
+    public void onMsgFromFragToMain(String sender, String strValue);
+}
